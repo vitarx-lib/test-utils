98a98d47821a4a0d6afeaced96889a0864e36e08
ZhuChongLin <8210856@qq.com>
2025-09-05 04:35:09 +0800

    refactor(export): 调整模块导出结构
    - 在主入口文件中调整了导出结构，将 createTestingApp 和 TestingApp 合并导出
    - 优化了 mount 函数和 Wrapper 类的导出方式
    - 保持了类型定义和工具函数的导出不变

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@
  * @file 测试工具主入口
  */
 
-export { createTestingApp } from './testingApp.js'
-export { mount } from './mount.js'
-export type { MountOptions, Wrapper, SpyFunction, SpyCall } from './types.js'
+export { createTestingApp, TestingApp } from './testingApp.js'
+export { mount, Wrapper } from './mount.js'
+export type { MountOptions, SpyFunction, SpyCall } from './types.js'
 export { nextTick, flushPromises, setValue } from './utils.js'
 export { createSpy, isSpy, getCalls, resetCalls } from './spy.js'
