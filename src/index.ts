c89a4bd5c389947867b9d3fac335bac1e0aff9e0
ZhuChongLin <8210856@qq.com>
2025-09-05 05:06:41 +0800

    refactor(src): 移除未使用的导出项
    - 从 src/index.ts 中删除了未使用的 setValue 导出
    - 优化了导出列表，保留了其他现有的导出项

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,5 @@
 export { createTestingApp, TestingApp } from './testingApp.js'
 export { mount, Wrapper } from './mount.js'
 export type { MountOptions, SpyFunction, SpyCall } from './types.js'
-export { nextTick, flushPromises, setValue } from './utils.js'
+export { nextTick, flushPromises } from './utils.js'
 export { createSpy, isSpy, getCalls, resetCalls } from './spy.js'
